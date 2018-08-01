6deefb8895c2c8fa44a4943dafcd8ed4e0fbf870
Eduardo Buchele <eduardobuchele@gmail.com>
2018-08-01 16:39:08 -0300

    Projeto Inicial Rodando
    Projeto Inicial Rodando

---
+++ casadocodigo-master/src/main/java/br/com/casadocodigo/loja/dao/UsuarioDAO.java
@@ -0,0 +1,36 @@
+package br.com.casadocodigo.loja.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Repository;
+
+import br.com.casadocodigo.loja.models.Usuario;
+
+@Repository
+public class UsuarioDAO implements UserDetailsService {
+
+	@PersistenceContext
+	private EntityManager manager;
+
+	@Override
+	public Usuario loadUserByUsername(String email) {
+		List<Usuario> usuarios = manager.createQuery("select u from Usuario u where u.email = :email", Usuario.class)
+				.setParameter("email", email).getResultList();
+
+		if (usuarios.isEmpty()) {
+			throw new UsernameNotFoundException("O usuário " + email + " não foi encontrado");
+		}
+
+		return usuarios.get(0);
+	}
+
+	public void gravar(Usuario usuario) {
+		manager.persist(usuario);
+	}
+
+}
---
+++ casadocodigo-master/src/main/java/br/com/casadocodigo/loja/models/Usuario.java
@@ -0,0 +1,103 @@
+package br.com.casadocodigo.loja.models;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Entity
+public class Usuario implements UserDetails {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private String email;
+
+	private String nome;
+
+	private String senha;
+
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "Usuario_Role", joinColumns = @JoinColumn(name = "email"), inverseJoinColumns = @JoinColumn(name = "role_nome"))
+	private List<Role> roles = new ArrayList<>();
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public List<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<Role> roles) {
+		this.roles = roles;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return this.roles;
+	}
+
+	@Override
+	public String getPassword() {
+		return this.senha;
+	}
+
+	@Override
+	public String getUsername() {
+		return this.email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+}
